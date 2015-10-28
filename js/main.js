ccfb4f18296d741f0816e04953c68128675dcfe2
victorgichohi <victorgichohi48@gmail.com>
2015-10-28 16:54:47 +0300

    commented out the fu nction that converts the csv to json

---
+++ js/main.js
@@ -0,0 +1,10 @@
+d3.csv("../refs.csv", function(d) {
+  return {
+    year: new Date(+d.Year, 0, 1), // convert "Year" column to Date
+    make: d.Make,
+    model: d.Model,
+    length: +d.Length // convert "Length" column to number
+  };
+}, function(error, rows) {
+  console.log(rows);
+});
